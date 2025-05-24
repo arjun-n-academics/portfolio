d4b9f4b936588350254b01386f7c347960d687d0
arjun-n-academics <arjun.n.academics@gmail.com>
2025-05-25 01:22:09 +0530

    Website Version 2 - Js

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ document.querySelectorAll('.copy-btn').forEach(button => {
         });
     });
 });
+
+
+const roles = ["Production Engineer.", "Web Designer.", "SAP Analyst."];
+  const typedText = document.getElementById("typed-text");
+
+  let roleIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+
+  function type() {
+    const currentRole = roles[roleIndex];
+    const displayedText = currentRole.substring(0, charIndex);
+    typedText.textContent = displayedText;
+
+    if (!isDeleting && charIndex < currentRole.length) {
+      charIndex++;
+      setTimeout(type, 100);
+    } else if (isDeleting && charIndex > 0) {
+      charIndex--;
+      setTimeout(type, 50);
+    } else {
+      isDeleting = !isDeleting;
+      if (!isDeleting) {
+        roleIndex = (roleIndex + 1) % roles.length;
+      }
+      setTimeout(type, 1000);
+    }
+  }
+
+  type();
